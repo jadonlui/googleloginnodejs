3bf7b695e786d573afc0deefa00310875f3ebb1b
jasonlui <cherngen6@gmail.com>
2022-12-09 00:51:50 +0800

    googleloginback

---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var router = express.Router();
+const passport = require("passport");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    session: false,
+  }),
+  (req, res) => {
+    console.log('data123')
+    res.json({
+      status: true,
+      data: {
+        id: req.user.id,
+        name: req.user.displayName,
+        account: req.user.emails[0].value,
+        //  userAll: req.user,
+      },
+    });
+  }
+);
+
+module.exports = router;
